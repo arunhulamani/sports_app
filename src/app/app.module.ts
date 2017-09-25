8d5a8eba46382f3516f874870cf78aec10b957a7
CODE1\310260801 <uday.hosamani@philips.com>
2017-09-25 14:54:42 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TeamComponent } from './team.component';
+import { HttpModule } from '@angular/http';
+import {HttpClientModule} from '@angular/common/http';
+import { TeamService } from './team.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeamComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [TeamService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/team.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {Http, Response} from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+
+@Injectable()
+export class TeamService {
+
+  constructor(private http: Http) { }
+
+  getTeam(): Observable<any> {
+    return this.http.get('./../assets/sports.json').map((response: Response) => response.json());
+  }
+
+}
